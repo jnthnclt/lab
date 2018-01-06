26eb01bcb1a7b7900963fd67bc4b75ed88b5bbc4
Jonathan Colt <jonathan.colt@thredup.com>
2018-01-06 10:41:05 -1000

    Fixed back Assert import

--- lab-consistency/src/test/java/com/github/jnthnclt/os/lab/consistency/QuorumIndexNGTest.java
+++ lab-consistency/src/test/java/com/github/jnthnclt/os/lab/consistency/QuorumIndexNGTest.java
@@ -1,9 +1,8 @@
 package com.github.jnthnclt.os.lab.consistency;
 
-import org.testng.annotations.Test;
-import sun.jvm.hotspot.utilities.Assert;
-
 import java.util.Arrays;
+import org.testng.annotations.Test;
+import org.testng.Assert;
 
 /**
  * Created by colt on 11/23/17.
@@ -14,26 +13,26 @@ public class QuorumIndexNGTest {
     public void testQI() throws Exception {
 
 
-        Assert.that(QuorumIndex.qi(new long[]{0, 0}) == null, "expected null");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{-1, -1, -1}), null), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{0, -1, -1}), null), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{-1, 0, -1}), null), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{-1, -1, 0}), null), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{-1, 0, 0}), new int[]{1, 2}), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{0, -1, 0}), new int[]{0, 2}), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{0, 0, -1}), new int[]{0, 1}), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{0, 0, 0}), new int[]{0, 1, 2}), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{0, 0, 1}), new int[]{0, 1}), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{0, 1, 0}), new int[]{0, 2}), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{1, 0, 0}), new int[]{1, 2}), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{1, 0, 1}), new int[]{0, 2}), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{1, 1, 0}), new int[]{0, 1}), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{0, 1, 1}), new int[]{1, 2}), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{1, 1, 1}), new int[]{0, 1, 2}), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{0, 1, 2}), null), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{0, 0, 1, 1}), null), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{0, 0, 1, 2}), null), "");
-        Assert.that(Arrays.equals(QuorumIndex.qi(new long[]{0, 0, 0, 1}), new int[]{0, 1, 2}), "");
+        Assert.assertTrue(QuorumIndex.qi(new long[]{0, 0}) == null, "expected null");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{-1, -1, -1}), null), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{0, -1, -1}), null), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{-1, 0, -1}), null), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{-1, -1, 0}), null), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{-1, 0, 0}), new int[]{1, 2}), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{0, -1, 0}), new int[]{0, 2}), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{0, 0, -1}), new int[]{0, 1}), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{0, 0, 0}), new int[]{0, 1, 2}), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{0, 0, 1}), new int[]{0, 1}), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{0, 1, 0}), new int[]{0, 2}), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{1, 0, 0}), new int[]{1, 2}), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{1, 0, 1}), new int[]{0, 2}), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{1, 1, 0}), new int[]{0, 1}), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{0, 1, 1}), new int[]{1, 2}), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{1, 1, 1}), new int[]{0, 1, 2}), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{0, 1, 2}), null), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{0, 0, 1, 1}), null), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{0, 0, 1, 2}), null), "");
+        Assert.assertTrue(Arrays.equals(QuorumIndex.qi(new long[]{0, 0, 0, 1}), new int[]{0, 1, 2}), "");
 
 
 //        System.out.println(Arrays.toString(QuorumIndex.qi(new long[] { 0, 0, 0 })) + "=2 ?");
